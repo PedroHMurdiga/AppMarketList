31074fa0d76646ec99029c31b20eb1f4306330cd
Usuario <Usuario@CPD02-M13>
2024-03-04 11:04:16 -0300

    Adicionar arquivos de projeto.

---
+++ AppMarketList/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMarketList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppMarketList/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppMarketList
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
